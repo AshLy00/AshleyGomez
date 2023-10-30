dd43c336db6031a9453cf21e93cf756ac9a2fea6
AshLy00 <ashleygomezbarraza@gmail.com>
2023-10-30 13:06:44 -0500

    hero in progress

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import TheHeader from "./components/app/TheHeader.vue";
+import TheHero from "./components/app/TheHero.vue";
 </script>
 
 <template>
   <TheHeader />
+  <TheHero />
   <main></main>
   <footer></footer>
 </template>
---
+++ src/components/app/TheHero.vue
@@ -0,0 +1,34 @@
+<script setup></script>
+
+<template>
+  <div class="hero_container">
+    <div>
+      <h1>ACA VA EL HERO XD</h1>
+      <p>Aca va mas texto que no tengo :c</p>
+    </div>
+
+    <img src="/Yop.png" />
+  </div>
+</template>
+
+<style scoped>
+.hero_container {
+  background-color: red;
+  height: 600px;
+  display: flex;
+  justify-content: space-between;
+}
+img {
+  width: 660px;
+  height: 600px;
+}
+@media screen and (max-width: 900px) {
+  .hero_container {
+    height: 200px;
+  }
+  img {
+    width: 260px;
+    height: 200px;
+  }
+}
+</style>
