b90c488fb9d73219e4b05154b4c9968d435c37b1
AshLy00 <ashleygomezbarraza@gmail.com>
2023-10-30 12:38:09 -0500

    header responsive

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,11 @@
 <script setup>
-import HelloWorld from './components/boton.vue';
+import TheHeader from "./components/app/TheHeader.vue";
 </script>
 
 <template>
-  <header>
-  <div class="header_items">
-    <h1>ASHLEY GÓMEZ</h1>
-    <div class="botones">
-      <button>PROJECTS</button>
-      <button>ABOUT</button>
-    </div>
-  </div>
-  </header>
+  <TheHeader />
+  <main></main>
+  <footer></footer>
 </template>
 
-<style scoped>
-header{
-  width:100%;
-  background-color: #6DB3E6;
-  color: white;
-}
-
-h1{
-  font-family: 'Rowdies', sans-serif;
-  font-size: 2rem;
-}
-
-button {
-  font-family: 'Aoboshi One', serif;
-  color: white;
-  background-color:#6DB3E6;
-  font-size: 0.8rem;
-}
-
-.header_items
-{
-  padding: 30px;
-  display:flex;
-  margin-right: 80px;
-  margin-left: 80px;
-  justify-content: space-between;
-  align-items: center;
-}
-
-.botones button{
-  margin: 20px;
-  padding: 10px;
-  border: solid;
-  border-radius: 30px;
-  width: 140px;
-  text-align:center;
-}
-</style>
-
-@media
+<style scoped></style>
---
+++ src/components/app/TheHeader.vue
@@ -0,0 +1,49 @@
+<script setup></script>
+
+<template>
+  <header>
+    <div class="header_items">
+      <h1>ASHLEY GÓMEZ</h1>
+      <div class="botones">
+        <button>PROJECTS</button>
+        <button>ABOUT</button>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+header {
+  width: 100%;
+  background-color: #6db3e6;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 110px;
+}
+
+.header_items {
+  width: 80%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 15px;
+}
+
+.botones {
+  display: flex;
+  gap: 15px;
+}
+
+@media screen and (max-width: 900px) {
+  header {
+    color: white;
+    height: 140px;
+  }
+
+  .header_items {
+    flex-direction: column;
+  }
+}
+</style>
