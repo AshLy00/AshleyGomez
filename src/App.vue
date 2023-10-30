3d016b321f4aa57b694aaa9cd2a06f0b3ec215e4
AshLy00 <ashleygomezbarraza@gmail.com>
2023-10-30 11:34:03 -0500

    header

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,22 @@ import HelloWorld from './components/boton.vue';
 </script>
 
 <template>
-  <div class="header">
+  <header>
+  <div class="header_items">
     <h1>Ashley Gómez</h1>
     <div class="botones">
       <p>Projects</p>
       <p>About</p>
     </div>
   </div>
+  </header>
 </template>
 
 <style scoped>
+header{
+  width:100%;
+  background-color: red;
+}
 h1{
   font-family: 'Rowdies', sans-serif;
   font-size: 2.5rem;
@@ -20,13 +26,13 @@ h1{
 p {
   font-family: 'Aoboshi One', serif;
 }
-.header
+.header_items
 {
-  display: flex;
-  justify-content: space-between;
-  margin: 40px;
+  padding: 20px;
+  display:flex;
   margin-right: 80px;
   margin-left: 80px;
+  justify-content: space-between;
   align-items: center;
 }
 .botones
