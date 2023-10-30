8640ee2c19f4de68eb5295c29fc85aebbc594a09
AshLy00 <ashleygomezbarraza@gmail.com>
2023-10-30 10:45:03 -0500

    start jiji

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,16 @@ import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <template>
-  <h1>Holas!</h1>
+  <h1>Ashley Gómez</h1>
+  <p>Projects</p>
+  <p>About</p>
 </template>
 
-<style scoped></style>
+<style scoped>
+h1{
+  font-family: 'Rowdies', sans-serif;
+}
+p {
+  font-family: 'Aoboshi One', serif;
+}
+</style>
