764958f520ef2c9e70352bf2b6bc4fa0fa47a85d
AshLy00 <ashleygomezbarraza@gmail.com>
2023-11-09 19:55:54 -0500

    uhy

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import TheHeader from "./components/app/TheHeader.vue";
 import TheHero from "./components/app/TheHero.vue";
+import TheIntro from "./components/app/TheIntro.vue";
 </script>
 
 <template>
   <TheHeader />
   <TheHero />
+  <TheIntro />
   <main></main>
   <footer></footer>
 </template>
---
+++ src/components/app/TheIntro.vue
@@ -0,0 +1,43 @@
+<script setup></script>
+
+<template>
+  <div class="intro_container">
+    <img src="BEDROOM.jpg" alt="">
+    <div class="intro_text_container">
+      <h1>PORTAFOLIO</h1>
+      <p>LOREM IPSUM DOLOR SIT AMET CONSECTETUR ADIPISCING ELIT LOREM IPSUM DOLOR SIT AMET CONSECTETUR ADIPISCING ELIT LOREM IPSUM DOLOR SIT AMET CONSECTETUR ADIPISCING</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.intro_container{
+    width:100%;
+    height:500px;
+    overflow:hidden;
+    position: relative;
+    display: inline-block;
+    display: flex;
+    align-items: center;
+}
+.intro_container img{
+    width:100%;
+}
+.intro_text_container{
+  position: absolute;
+  top:100px;
+  left:15%;
+  color:var(--color-white);
+  width: 70%;
+  text-align: center;
+  gap: 20px;
+}
+
+.intro_text_container h1{
+font-size: 10rem;
+}
+
+.intro_text_container p{
+font-size: 1.5rem;
+}
+</style>
