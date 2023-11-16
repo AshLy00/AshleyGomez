08752ea0211116672b637e26bbce4bcd03d44c16
AshLy00 <ashleygomezbarraza@gmail.com>
2023-11-16 08:58:50 -0500

    hero responsive in progress

--- src/components/app/TheHero.vue
+++ src/components/app/TheHero.vue
@@ -4,11 +4,17 @@
   <div class="hero_container">
     <div class="hero_text_container">
       <h1>LOREM IPSUM DOLOR SIT AMET CONSECTETUR ADIPISCING ELIT</h1>
-      <p>LOREM IPSUM DOLOR SIT AMET CONSECTETUR ADIPISCING ELIT LOREM IPSUM DOLOR SIT AMET CONSECTETUR ADIPISCING ELIT
-        LOREM IPSUM DOLOR SIT AMET CONSECTETUR ADIPISCING</p>
+      <p>
+        LOREM IPSUM DOLOR SIT AMET CONSECTETUR ADIPISCING ELIT LOREM IPSUM DOLOR
+        SIT AMET CONSECTETUR ADIPISCING ELIT LOREM IPSUM DOLOR SIT AMET
+        CONSECTETUR ADIPISCING
+      </p>
     </div>
     <div class="hero_img_container">
-      <img src="https://drive.google.com/uc?export=view&id=1UnBSbdjNL3H9ZFDY8AA5gybePzQm5RA9" alt="">
+      <img
+        src="https://drive.google.com/uc?export=view&id=1UnBSbdjNL3H9ZFDY8AA5gybePzQm5RA9"
+        alt=""
+      />
     </div>
   </div>
 </template>
@@ -23,7 +29,6 @@
 }
 
 .hero_img_container {
-  border: 1px solid transparent;
   align-self: end;
 }
 
@@ -34,7 +39,6 @@
 }
 
 .hero_text_container {
-  border: 1px solid transparent;
   position: relative;
   left: 100px;
 }
@@ -46,5 +50,126 @@
 .hero_text_container p {
   font-size: 1.5rem;
   width: 70%;
+}
+
+@media screen and (max-width: 1350px) {
+  .hero_text_container h1 {
+    font-size: 3rem;
+  }
+
+  .hero_text_container p {
+    font-size: 1.2rem;
+  }
+
+  .hero_container {
+    height: 550px;
+  }
+
+  .hero_img_container img {
+    width: 520px;
+  }
+}
+
+@media screen and (max-width: 1160px) {
+  .hero_text_container h1 {
+    font-size: 2.5rem;
+  }
+
+  .hero_text_container p {
+    font-size: 1rem;
+  }
+
+  .hero_container {
+    height: 450px;
+  }
+
+  .hero_img_container img {
+    width: 480px;
+  }
+}
+
+@media screen and (max-width: 970px) {
+  .hero_text_container h1 {
+    font-size: 2.1rem;
+  }
+
+  .hero_text_container p {
+    font-size: 0.8rem;
+  }
+
+  .hero_container {
+    height: 350px;
+  }
+
+  .hero_img_container img {
+    width: 380px;
+  }
+}
+
+@media screen and (max-width: 900px) {
+  .hero_text_container h1 {
+    font-size: 2.1rem;
+  }
+
+  .hero_text_container p {
+    font-size: 0.8rem;
+  }
+
+  .hero_container {
+    height: 340px;
+  }
+
+  .hero_img_container img {
+    width: 350px;
+  }
+
+  .hero_text_container {
+    left: 73px;
+  }
+}
+
+@media screen and (max-width: 770px) {
+  .hero_text_container h1 {
+    font-size: 1.9rem;
+  }
+
+  .hero_text_container p {
+    font-size: 0.5rem;
+  }
+
+  .hero_container {
+    height: 280px;
+  }
+
+  .hero_img_container img {
+    width: 310px;
+  }
+
+  .hero_text_container {
+    left: 60px;
+  }
+}
+
+@media screen and (max-width: 670px) {
+  .hero_text_container h1 {
+    font-size: 1.5rem;
+  }
+
+  .hero_text_container p {
+    font-size: 0.5rem;
+  }
+
+  .hero_container {
+    height: 230px;
+  }
+
+  .hero_img_container img {
+    width: 250px;
+    right: 20px;
+  }
+
+  .hero_text_container {
+    left: 60px;
+  }
 }
 </style>
