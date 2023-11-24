ba10e444827b02526144831deb31a2a0fc9ecc1f
AshLy00 <ashleygomezbarraza@gmail.com>
2023-11-23 22:23:46 -0500

    gallery filters

---
+++ src/components/app/TheFilterButtons.vue
@@ -0,0 +1,20 @@
+<script setup></script>
+<template>
+<div class="single_filter">
+<p>ILUSTRACIÓN</p>
+</div>
+</template>
+
+
+<style scoped>
+.single_filter{
+    background-color:var(--color-pink) ;
+    height:50px;
+    width: 180px;
+    align-items: center;
+    display: flex;
+    justify-content: center;
+    border-radius: 30px;
+    color: var(--color-white);
+}
+</style>
--- src/components/app/TheGallery.vue
+++ src/components/app/TheGallery.vue
@@ -1,7 +1,15 @@
 <script setup>
 import TheGalleryItem from './TheGalleryItem.vue';
+import TheFilterButtons from './TheFilterButtons.vue';
 </script>
 <template>
+    <div class="filters_buttons">
+        <TheFilterButtons />
+        <TheFilterButtons />
+        <TheFilterButtons />
+        <TheFilterButtons />
+        <TheFilterButtons />
+    </div>
 <div class="gallery_grid">
 <TheGalleryItem />
 <TheGalleryItem />
@@ -13,11 +21,18 @@ import TheGalleryItem from './TheGalleryItem.vue';
 
 
 <style scoped>
+.filters_buttons{
+    display: flex;
+    gap: 20px;
+    height: 100px;
+    align-items: center;
+    justify-content: center;
+}
 .gallery_grid{
     display: grid;
 grid-template-columns: 1fr 1fr 1fr 1fr;
 gap:20px;
-margin-top: 50px;
+margin-top: 20px;
 margin-bottom: 50px;
 }
 </style>
