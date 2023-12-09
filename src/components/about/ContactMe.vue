393f98a0c2b34f4df7d885b2673c724878380473
AshLy00 <140760135+AshLy00@users.noreply.github.com>
2023-12-08 20:42:16 -0500

    test form

--- src/components/about/ContactMe.vue
+++ src/components/about/ContactMe.vue
@@ -25,11 +25,7 @@
       <textarea id="message" name="message" rows="4" required></textarea>
 
       <button type="submit">Submit</button>
-      <input
-        type="hidden"
-        name="_next"
-        value="https://ashley-gomez.vercel.app/"
-      />
+      <input type="hidden" name="_next" value="" />
       <input type="hidden" name="_captcha" value="false" />
     </form>
   </div>
